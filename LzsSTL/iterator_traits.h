3f4e80064a0e6de0324b0d70de3427a22924f741
Rango-lzs <1191472748@qq.com>
2021-06-23 15:54:01 +0800

    add build in type traits

--- LzsSTL/iterator_traits.h
+++ LzsSTL/iterator_traits.h
@@ -1,5 +1,5 @@
-#ifndef TYPE_TRAITS_H
-#define TYPE_TRAITS_H
+#ifndef ITERATOR_TRAITS_H
+#define ITERATOR_TRAITS_H
 
 //__type_traits can get the iterator's asociate type  by nested typename definitin
 // but when the iterator is raw pointer, it should be partial specified
@@ -12,12 +12,12 @@ struct iterator_traits{
     typedef typename    iterator::const_pointer_type        const_pointer_type;
     typedef typename    iterator::reference_type            reference_type;
     typedef typename    iterator::const_reference_type      const_reference_type;
-    typedef typename    iteartor::pointer_different_type    pointer_different_type;
+    typedef typename    iterator::pointer_different_type    pointer_different_type;
 };
 
 
 //partial specification for raw pointer and const raw pointer
-template <T>
+template <typename T>
 struct iterator_traits<T*>{
     typedef typename    T           value_type;
     typedef typename    T*          pointer_type;
@@ -28,7 +28,7 @@ struct iterator_traits<T*>{
     typedef typename    ptrdiff_t   pointer_different_type; 
 };
 
-template <T>
+template <typename T>
 struct iterator_traits<const T*>{
     typedef typename    T           value_type;
     typedef typename    T*          pointer_type;
