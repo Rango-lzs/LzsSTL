2a946aeae6252b00d8a55cdfc05d4a7b741f25f3
Rango-lzs <1191472748@qq.com>
2021-06-23 15:54:01 +0800

    add build in type traits

--- LzsSTL/iterator_traits.h
+++ LzsSTL/iterator_traits.h
@@ -1,5 +1,5 @@
-#ifndef TYPE_TRAITS_H
-#define TYPE_TRAITS_H
+#ifndef ITERATOR_TRAITS_H
+#define ITERATOR_TRAITS_H
 
 //__type_traits can get the iterator's asociate type  by nested typename definitin
 // but when the iterator is raw pointer, it should be partial specified
